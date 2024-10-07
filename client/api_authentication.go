80d38f6679ac162985b7a19f99534d5e0dd5c220
ramanan-ravi <ramanan@deepfence.io>
2024-10-07 23:21:16 +0530

    Update sdk

--- client/api_authentication.go
+++ client/api_authentication.go
@@ -3,7 +3,7 @@ Deepfence ThreatStryker
 
 Deepfence Runtime API provides programmatic control over Deepfence microservice securing your container, kubernetes and cloud deployments. The API abstracts away underlying infrastructure details like cloud provider,  container distros, container orchestrator and type of deployment. This is one uniform API to manage and control security alerts, policies and response to alerts for microservices running anywhere i.e. managed pure greenfield container deployments or a mix of containers, VMs and serverless paradigms like AWS Fargate.
 
-API version: v2.3.0
+API version: v2.4.0
 Contact: community@deepfence.io
 */
 
@@ -1112,7 +1112,7 @@ func (a *AuthenticationAPIService) SsoLoginExecute(r ApiSsoLoginRequest) (*http.
 		return nil, reportError("email is required and must be specified")
 	}
 
-	parameterAddToHeaderOrQuery(localVarQueryParams, "email", r.email, "")
+	parameterAddToHeaderOrQuery(localVarQueryParams, "email", r.email, "form", "")
 	// to determine the Content-Type header
 	localVarHTTPContentTypes := []string{}
 
